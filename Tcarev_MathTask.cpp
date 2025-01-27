6add9877152e4169e0166002568fcddba8de8545
TcarevVK <tsarev325vova@gmail.com>
2025-01-27 16:11:59 +0300

    Create void menu

---
+++ Tcarev_MathTask.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Tcarev_MathTask.h"
+
+int main()
+{
+    string str_input; //Вспомогательная переменная.
+
+    //Ввод ширины прямоугольника.
+    cout << "inpute height A" << endl;
+    //Ввод значения в текстовом виде.
+    getline(cin, str_input);
+    //Пока ввод некорректен( вводимое значение невозможно преобразовать в int)
+    while (!UserInput(str_input)) {
+        cout << "input height A" << endl;
+        //Повторный ввод данных.
+        getline(cin, str_input);
+    }
+
+    //Присвоение переменной NumberA преобразованного в тип int
+    //правильно введённого текстого значения.
+    int NumberA = stoi(str_input);
+    //Ввод высоты прямоугольника.
+    cout << "input weight B" << endl;
+    getline(cin, str_input);
+    while (!UserInput(str_input)) {
+        cout << "input weight B" << endl;
+        getline(cin, str_input);
+    }
+    
+    //Присвоение переменной NumberB преобразованного в тип int
+    //правильного введённого текстового значения.
+    int NumberB = stoi(str_input);
+
+    //Вычислене площади прямоугольника.
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+    //Вывод значение площади
+    cout << "Area of Rectangle is " << RectangleArea << endl;
+}
